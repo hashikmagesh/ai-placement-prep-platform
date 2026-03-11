45fe0dc6ccb4207bea7c79705c42487e1f57e6a0
hashikmagesh <hashikmagesh@gmail.com>
2026-03-11 15:33:04 +0530

    switch to sqlite for render deployment

--- placement/settings.py
+++ placement/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'placement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'placement',
-        'USER':'root',
-        'PASSWORD' :'1234',
-        'HOST' :'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
